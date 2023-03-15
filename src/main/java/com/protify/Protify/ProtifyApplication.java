f24916886e4d1483e9c357df7297a136da55aa6d
Ihavegot <lukasz.bocianski1112@gmail.com>
2023-03-15 20:07:23 +0100

    Project initialization

---
+++ src/main/java/com/protify/Protify/ProtifyApplication.java
@@ -0,0 +1,13 @@
+package com.protify.Protify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protify/Protify/ProtifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protify.Protify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
