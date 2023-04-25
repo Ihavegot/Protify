c6a7a78f27af8c5e3acf362897af38549c3b16a4
Dregdil <65603166+Dregdil@users.noreply.github.com>
2023-04-25 12:32:07 +0200

    test data
    added test data to models

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,296 @@
-INSERT INTO songs (id, title, artist_id) VALUES (1, 'Never gonna give you up', 1);
-INSERT INTO songs (id, title, artist_id) VALUES (2, 'Renai circulation', 2);
+-- Dla tabeli User
+INSERT INTO User (email, login, password) VALUES 
+('john@example.com', 'john123', 'password123'),
+('alice@example.com', 'alice123', 'password123'),
+('bob@example.com', 'bob123', 'password123'),
+('jane@example.com', 'jane123', 'password123'),
+('mark@example.com', 'mark123', 'password123'),
+('sarah@example.com', 'sarah123', 'password123'),
+('peter@example.com', 'peter123', 'password123'),
+('emily@example.com', 'emily123', 'password123'),
+('david@example.com', 'david123', 'password123'),
+('anna@example.com', 'anna123', 'password123'),
+('mike@example.com', 'mike123', 'password123'),
+('amy@example.com', 'amy123', 'password123'),
+('tom@example.com', 'tom123', 'password123'),
+('lisa@example.com', 'lisa123', 'password123'),
+('eric@example.com', 'eric123', 'password123'),
+('chris@example.com', 'chris123', 'password123'),
+('kate@example.com', 'kate123', 'password123'),
+('julia@example.com', 'julia123', 'password123'),
+('sam@example.com', 'sam123', 'password123'),
+('olivia@example.com', 'olivia123', 'password123'),
+('max@example.com', 'max123', 'password123'),
+('lucas@example.com', 'lucas123', 'password123'),
+('leo@example.com', 'leo123', 'password123'),
+('emma@example.com', 'emma123', 'password123'),
+('jake@example.com', 'jake123', 'password123'),
+('matt@example.com', 'matt123', 'password123'),
+('jennifer@example.com', 'jennifer123', 'password123'),
+('ava@example.com', 'ava123', 'password123'),
+('benjamin@example.com', 'benjamin123', 'password123'),
+('daniel@example.com', 'daniel123', 'password123'),
+('victoria@example.com', 'victoria123', 'password123'),
+('natalie@example.com', 'natalie123', 'password123'),
+('alex@example.com', 'alex123', 'password123'),
+('anna@example.com', 'anna123', 'password123'),
+('maya@example.com', 'maya123', 'password123'),
+('paul@example.com', 'paul123', 'password123'),
+('jason@example.com', 'jason123', 'password123'),
+('george@example.com', 'george123', 'password123'),
+('sophia@example.com', 'sophia123', 'password123'),
+('lily@example.com', 'lily123', 'password123'),
+('owen@example.com', 'owen123', 'password123'),
+('jacob@example.com', 'jacob123', 'password123'),
+('zoey@example.com', 'zoey123', 'password123'),
+('charlotte@example.com', 'charlotte123', 'password123'),
+('violet@example.com', 'violet123', 'password123'),
+('liam@example.com', 'liam123', 'password123'),
+('ella@example.com', 'ella123', 'password123'),
+('avery@example.com', 'avery123', 'password123'),
+('madison@example.com', 'madison123', 'password123'),
+('jayden@example.com', 'jayden123', 'password123');
+
+-- Dla tabeli Artist
+INSERT INTO Artist (artistName, name, surname) VALUES
+('AC/DC', 'Malcolm', 'Young'),
+('AC/DC', 'Angus', 'Young'),
+('The Beatles', 'John', 'Lennon'),
+('The Beatles', 'Paul', 'McCartney'),
+('The Beatles', 'George', 'Harrison'),
+('The Beatles', 'Ringo', 'Starr'),
+('Queen', 'Freddie', 'Mercury'),
+('Queen', 'Brian', 'May'),
+('Queen', 'John', 'Deacon'),
+('Queen', 'Roger', 'Taylor'),
+('Michael Jackson', 'Michael', 'Jackson'),
+('Madonna', 'Madonna', 'Ciccone'),
+('Prince', 'Prince', 'Rogers Nelson'),
+('David Bowie', 'David', 'Bowie'),
+('Elvis Presley', 'Elvis', 'Presley'),
+('Led Zeppelin', 'Jimmy', 'Page'),
+('Led Zeppelin', 'Robert', 'Plant'),
+('Led Zeppelin', 'John', 'Bonham'),
+('Led Zeppelin', 'John Paul', 'Jones'),
+('Pink Floyd', 'David', 'Gilmour'),
+('Pink Floyd', 'Roger', 'Waters'),
+('Pink Floyd', 'Syd', 'Barrett'),
+('Nirvana', 'Kurt', 'Cobain'),
+('Nirvana', 'Krist', 'Novoselic'),
+('Nirvana', 'Dave', 'Grohl'),
+('Metallica', 'James', 'Hetfield'),
+('Metallica', 'Lars', 'Ulrich'),
+('Metallica', 'Kirk', 'Hammett'),
+('Metallica', 'Robert', 'Trujillo'),
+('The Rolling Stones', 'Mick', 'Jagger'),
+('The Rolling Stones', 'Keith', 'Richards'),
+('The Rolling Stones', 'Charlie', 'Watts'),
+('The Rolling Stones', 'Ronnie', 'Wood'),
+('U2', 'Bono', 'Vox'),
+('U2', 'The Edge', 'Evans'),
+('U2', 'Adam', 'Clayton'),
+('U2', 'Larry', 'Mullen Jr.'),
+('Pearl Jam', 'Eddie', 'Vedder'),
+('Pearl Jam', 'Stone', 'Gossard'),
+('Pearl Jam', 'Jeff', 'Ament'),
+('Pearl Jam', 'Mike', 'McCready'),
+('Green Day', 'Billie', 'Joe Armstrong'),
+('Green Day', 'Mike', 'Dirnt'),
+('Green Day', 'Tré', 'Cool'),
+('Radiohead', 'Thom', 'Yorke'),
+('Radiohead', 'Jonny', 'Greenwood'),
+('Radiohead', 'Colin', 'Greenwood'),
+('Radiohead', 'Philip', 'Selway'),
+('The Doors', 'Jim', 'Morrison'),
+('The Doors', 'Ray', 'Manzarek'),
+('The Doors', 'Robby', 'Krieger'),
+('The Doors', 'John', 'Densmore');
+
+--dla tabeli Songs
+INSERT INTO Songs (title, artist_id, songFile) VALUES
+('Shape of You', 1, null),
+('Thinking Out Loud', 1, null),
+('Perfect', 1, null),
+('Photograph', 1, null),
+('Castle on the Hill', 1, null),
+('Someone Like You', 2, null),
+('Hello', 2, null),
+('Rolling in the Deep', 2, null),
+('Set Fire to the Rain', 2, null),
+('Skyfall', 2, null),
+('Thriller', 3, null),
+('Beat It', 3, null),
+('Billie Jean', 3, null),
+('Smooth Criminal', 3, null),
+('Black or White', 3, null),
+('Bohemian Rhapsody', 4, null),
+('We Will Rock You', 4, null),
+('Don''t Stop Me Now', 4, null),
+('Somebody to Love', 4, null),
+('Another One Bites the Dust', 4, null),
+('Livin'' on a Prayer', 5, null),
+('You Give Love a Bad Name', 5, null),
+('Wanted Dead or Alive', 5, null),
+('It''s My Life', 5, null),
+('Always', 5, null),
+('Stairway to Heaven', 6, null),
+('Whole Lotta Love', 6, null),
+('Immigrant Song', 6, null),
+('Black Dog', 6, null),
+('Kashmir', 6, null),
+('Smells Like Teen Spirit', 7, null),
+('Come as You Are', 7, null),
+('Lithium', 7, null),
+('Heart-Shaped Box', 7, null),
+('In Bloom', 7, null),
+('Californication', 8, null),
+('Under the Bridge', 8, null),
+('Scar Tissue', 8, null),
+('Give It Away', 8, null),
+('Can''t Stop', 8, null),
+('Lose Yourself', 9, null),
+('Without Me', 9, null),
+('The Real Slim Shady', 9, null),
+('Love the Way You Lie', 9, null),
+('Mockingbird', 9, null),
+('What''s My Name?', 10, null),
+('Umbrella', 10, null),
+('Diamonds', 10, null),
+('Work', 10, null),
+('We Found Love', 10, null);
+
+ -- dla tabeli score
+ INSERT INTO Score (user_id, songs_id, score) VALUES
+ (7, 28, 4.5),
+ (2, 14, 3.2),
+ (1, 49, 4.8),
+ (3, 20, 2.7),
+ (4, 31, 4.1),
+ (6, 43, 3.9),
+ (5, 26, 3.6),
+ (3, 37, 2.3),
+ (7, 9, 4.7),
+ (2, 22, 3.8),
+ (1, 46, 4.5),
+ (4, 5, 3.1),
+ (6, 40, 3.6),
+ (5, 16, 2.9),
+ (3, 33, 2.5),
+ (7, 47, 4.2),
+ (2, 24, 3.5),
+ (1, 41, 4.9),
+ (4, 29, 3.7),
+ (6, 19, 3.4),
+ (5, 13, 2.2),
+ (3, 48, 2.8),
+ (7, 12, 4.3),
+ (2, 42, 3.3),
+ (1, 27, 4.4),
+ (4, 39, 3.0),
+ (6, 21, 3.7),
+ (5, 8, 2.6),
+ (3, 25, 2.9),
+ (7, 36, 4.1),
+ (2, 44, 3.8),
+ (1, 15, 4.5),
+ (4, 30, 3.3),
+ (6, 23, 3.5),
+ (5, 18, 2.8),
+ (3, 11, 2.2),
+ (7, 45, 4.6),
+ (2, 35, 3.6),
+ (1, 10, 4.9),
+ (4, 50, 3.4),
+ (6, 17, 3.1),
+ (5, 32, 2.4),
+ (3, 38, 2.7),
+ (7, 6, 4.0),
+ (2, 1, 3.9),
+ (1, 4, 4.7),
+ (4, 2, 3.2),
+ (6, 3, 3.8),
+ (5, 7, 2.6),
+ (3, 34, 2.5);
+--playlist
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+INSERT INTO Playlist (user_id) VALUES (1);
+INSERT INTO Playlist (user_id) VALUES (2);
+INSERT INTO Playlist (user_id) VALUES (3);
+INSERT INTO Playlist (user_id) VALUES (4);
+INSERT INTO Playlist (user_id) VALUES (5);
+--many to many
+INSERT INTO playlist_songs (playlist_id, songs_id)
+VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(3, 1),
+(3, 3),
+(3, 5),
+(4, 2),
+(4, 4),
+(4, 6),
+(5, 1),
+(5, 4),
+(5, 5),
+(6, 2),
+(6, 3),
+(6, 6),
+(7, 1),
+(7, 2),
+(7, 5),
+(8, 3),
+(8, 4),
+(8, 6),
+(9, 1),
+(9, 6),
+(10, 2),
+(10, 4),
+(10, 5);
