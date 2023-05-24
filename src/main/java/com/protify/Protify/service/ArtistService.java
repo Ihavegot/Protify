9eabeabe37fa78b5d79670c1d0dc4848a9a5196f
Dregdil <65603166+Dregdil@users.noreply.github.com>
2023-05-24 14:07:03 +0200

    added artist endpoint

--- src/main/java/com/protify/Protify/service/ArtistService.java
+++ src/main/java/com/protify/Protify/service/ArtistService.java
@@ -1,6 +1,8 @@
 package com.protify.Protify.service;
 
+import com.protify.Protify.models.Artist;
 import com.protify.Protify.models.Songs;
+import com.protify.Protify.repository.ArtistRepository;
 import com.protify.Protify.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,16 +14,13 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class SongService {
-    private final SongRepository songRepository;
-    public Page<Songs> getSongs(Pageable page){
-        return songRepository.findAll(page);
+public class ArtistService {
+    private final ArtistRepository artistRepository;
+    public Page<Artist> getArtist(Pageable page){
+        return artistRepository.findAll(page);
     }
 
-    public Optional<Songs> getSingleSong(long id){
-        return songRepository.findById(id);
-    }
-
-    public Page<Songs> getSongsByArtist(Long id, Pageable page) {return  songRepository.findByArtistId(id, page);
+    public Optional<Artist> getSingleArtist(long id){
+        return artistRepository.findById(id);
     }
 }
