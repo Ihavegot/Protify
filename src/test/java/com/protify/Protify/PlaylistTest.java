d7245aac29a874ad0139a313f78f8fa5925e79d4
Ihavegot <lukasz.bocianski1112@gmail.com>
2023-05-29 12:14:15 +0200

    (fix) Playlist tests

--- src/test/java/com/protify/Protify/PlaylistTest.java
+++ src/test/java/com/protify/Protify/PlaylistTest.java
@@ -7,10 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.protify.Protify.models.Artist;
-import com.protify.Protify.models.Entry;
 import com.protify.Protify.models.Playlist;
-import com.protify.Protify.models.Song;
-import com.protify.Protify.repository.EntryRepository;
+import com.protify.Protify.models.Songs;
 import com.protify.Protify.repository.PlaylistRepository;
 import com.protify.Protify.repository.SongRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -53,21 +51,20 @@ class PlaylistTest {
 
 
 
-        mvc.perform(get("/playlists")      .accept(MediaType.APPLICATION_JSON))
+        mvc.perform(get("/playlist")      .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.page.size").value(20))
                 .andExpect(jsonPath("$.page.totalElements").value(50))
                 .andExpect(jsonPath("$.page.totalPages").value(3))
                 .andExpect(jsonPath("$.page.number").value(0))
-                .andExpect(jsonPath("$._links.self.href").value("http://localhost/playlists"))
-                .andExpect(jsonPath("$._links.first.href").value("http://localhost/playlists?page=0&size=20"))
-                .andExpect(jsonPath("$._links.next.href").value("http://localhost/playlists?page=1&size=20"))
-                .andExpect(jsonPath("$._links.last.href").value("http://localhost/playlists?page=2&size=20"))
-                .andExpect(jsonPath("$._links.profile.href").value("http://localhost/profile/playlists"))
-                .andExpect(jsonPath("$._embedded.playlists").isArray())
-                .andExpect(jsonPath("$._embedded.playlists", hasSize(20)))
-                .andExpect(jsonPath("$._embedded.playlists[3]._links.entries.href").value("http://localhost/playlists/"+entities.get(3).getId()+"/entries"))
-                .andExpect(jsonPath("$._embedded.playlists[3]._links.self.href").value("http://localhost/playlists/"+entities.get(3).getId()))
+                .andExpect(jsonPath("$._links.self.href").value("http://localhost/playlist?page=0&size=20"))
+                .andExpect(jsonPath("$._links.first.href").value("http://localhost/playlist?page=0&size=20"))
+                .andExpect(jsonPath("$._links.next.href").value("http://localhost/playlist?page=1&size=20"))
+                .andExpect(jsonPath("$._links.last.href").value("http://localhost/playlist?page=2&size=20"))
+                .andExpect(jsonPath("$._embedded.playlistList").isArray())
+                .andExpect(jsonPath("$._embedded.playlistList", hasSize(20)))
+                .andExpect(jsonPath("$._embedded.playlistList[3]._links.songs.href").value("http://localhost/playlist/"+entities.get(3).getId()+"/song"))
+                .andExpect(jsonPath("$._embedded.playlistList[3]._links.self.href").value("http://localhost/playlist/"+entities.get(3).getId()))
 
         ;
     }
@@ -80,11 +77,11 @@ class PlaylistTest {
 
 
 
-        mvc.perform(get("/playlists/"+entities.get(5).getId())      .accept(MediaType.APPLICATION_JSON))
+        mvc.perform(get("/playlist/"+entities.get(5).getId())      .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$._links.self.href").value("http://localhost/playlists/"+entities.get(5).getId()))
-                .andExpect(jsonPath("$._links.entries.href").value("http://localhost/playlists/"+entities.get(5).getId()+"/entries"))
-                .andExpect(jsonPath("$._links.self.href").value("http://localhost/playlists/"+entities.get(5).getId()))
+                .andExpect(jsonPath("$._links.self.href").value("http://localhost/playlist/"+entities.get(5).getId()))
+                .andExpect(jsonPath("$._links.songs.href").value("http://localhost/playlist/"+entities.get(5).getId()+"/song"))
+                .andExpect(jsonPath("$._links.self.href").value("http://localhost/playlist/"+entities.get(5).getId()))
 
         ;
     }
