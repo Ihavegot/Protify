5a6a85bba37398908d053c370bfd960e3caee5bd
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-06-01 18:22:04 +0200

    test(HomeController): test hops with traverson

--- src/test/java/com/protify/Protify/controllers/HomeControllerTest.java
+++ src/test/java/com/protify/Protify/controllers/HomeControllerTest.java
@@ -6,29 +6,58 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.protify.Protify.ProtifyApplication;
+import com.protify.Protify.models.Songs;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.client.Hop;
 import org.springframework.hateoas.client.Traverson;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.net.URI;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ProtifyApplication.class)
 @AutoConfigureMockMvc
 class HomeControllerTest {
  @Autowired
     private MockMvc mvc;
 
-    @Test
-    void getHome() throws Exception {
 
+    @Value(value="${local.server.port}")
+    private int port;
+    private Traverson traverson;
 
+    @BeforeEach
+    public void beforeEach(){
+        traverson = new Traverson(URI.create("http://localhost:"+port+"/"), MediaTypes.HAL_JSON);
 
-           mvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
-      .andExpect(status().isOk())
-             .andExpect(jsonPath("$._links.songs.href").value("http://localhost/songs{?page,size,sort}"))
-             .andExpect(jsonPath("$._links.playlists.href").value("http://localhost/playlists{?page,size,sort}"));
     }
+
+    @ParameterizedTest
+    @CsvSource({"songs","playlists"})
+    void links(String rel) throws Exception {
+
+        PagedModel<Object> page = traverson.follow(Hop.rel(rel)
+                        .withParameter("page", 1)
+                        .withParameter("sort", "id")
+                        .withParameter("size", 30))
+                .toObject(new ParameterizedTypeReference<PagedModel<Object>>() {
+                });
+
+        Assertions.assertNotNull(page);
+
+    }
+
+
 }
