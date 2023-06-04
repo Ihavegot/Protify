b7df919f87c3168b7679e1fd631d251665274ef9
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-06-04 20:42:06 +0200

    feat: enable hal forms

--- src/main/java/com/protify/Protify/ProtifyApplication.java
+++ src/main/java/com/protify/Protify/ProtifyApplication.java
@@ -2,8 +2,11 @@ package com.protify.Protify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type=EnableHypermediaSupport.HypermediaType.HAL_FORMS)
+
 public class ProtifyApplication {
 
 	public static void main(String[] args) {
