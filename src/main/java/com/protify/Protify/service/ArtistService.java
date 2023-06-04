7449755f5a169133c77f7de3e0b7b45af7763195
Dregdil <65603166+Dregdil@users.noreply.github.com>
2023-06-04 20:11:20 +0200

    Added aristist (PUT POST DELETE) to endpoint

--- src/main/java/com/protify/Protify/service/ArtistService.java
+++ src/main/java/com/protify/Protify/service/ArtistService.java
@@ -1,6 +1,7 @@
 package com.protify.Protify.service;
 
 import com.protify.Protify.models.Artist;
+import com.protify.Protify.models.Playlist;
 import com.protify.Protify.models.Songs;
 import com.protify.Protify.repository.ArtistRepository;
 import com.protify.Protify.repository.SongRepository;
@@ -22,5 +23,29 @@ public class ArtistService {
 
     public Optional<Artist> getSingleArtist(long id){
         return artistRepository.findById(id);
+    }
+
+    public Artist addSingleArtist(Artist artist){
+        return artistRepository.save(artist);
+    }
+
+    public Optional<Artist> updateSingleArtist(Artist artist){
+        return artistRepository.findById(artist.getId()).map(b -> {
+            if (artist.getArtistName() != null) {
+                b.setArtistName(artist.getArtistName());
+            }
+            if(artist.getName() != null){
+                b.setName(artist.getName());
+            }
+            if(artist.getSurname() != null)
+            {
+                b.setSurname(artist.getSurname());
+            }
+            return artistRepository.save(b);
+        });
+    }
+
+    public void deleteSingleArtist(long id){
+        artistRepository.deleteById(id);
     }
 }
