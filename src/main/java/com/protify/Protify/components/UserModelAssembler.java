f8c464698441ad966660853a06b47212d73f5513
Ihavegot <lukasz.bocianski1112@gmail.com>
2023-06-06 18:29:45 +0200

    fix usermodelassembler test

--- src/main/java/com/protify/Protify/components/UserModelAssembler.java
+++ src/main/java/com/protify/Protify/components/UserModelAssembler.java
@@ -29,9 +29,9 @@ public class UserModelAssembler implements RepresentationModelAssembler<User, En
     public EntityModel<User> toModel(User entity) {
         HalModelBuilder builder = HalModelBuilder.halModelOf(entity)
                 .link(links.linkToItemResource(entity, User::getId)
+                        .andAffordance(afford(methodOn(UserController.class).deleteUser(entity, null)))
                         .andAffordance(afford(methodOn(UserController.class).patchUser(entity, null, null)))
                         .andAffordance(afford(methodOn(UserController.class).putUser(entity.getId(), null, null)))
-                        .andAffordance(afford(methodOn(UserController.class).deleteUser(entity, null)))
                 )
                 .link(linkTo(methodOn(UserController.class).getPlaylists(entity.getId(), null, null, null, null)).withRel(
                         linkRelationProvider.getCollectionResourceRelFor(Playlist.class)
