17601e03ef7ef2cf08bf9c198ff2e6072c754c51
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-06-06 22:51:53 +0200

    feat: produce `hal-forms+json` `hal+json` (#39)
    * feat: produce `hal-forms+json` `hal+json`
    
    * chore: set `hal+json` as default

--- src/main/java/com/protify/Protify/controllers/ArtistController.java
+++ src/main/java/com/protify/Protify/controllers/ArtistController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/artist")
+@RequestMapping(value="/artist", produces = {MediaTypes.HAL_JSON_VALUE, MediaTypes.HAL_FORMS_JSON_VALUE})
 @ExposesResourceFor(Artist.class)
 public class ArtistController {
     private final ArtistService artistService;
