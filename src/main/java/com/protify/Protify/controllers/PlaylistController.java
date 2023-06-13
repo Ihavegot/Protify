7e1b62d4358d3142d1cdbc3d01c26743d94c6784
Łukasz Bociański <72608899+Ihavegot@users.noreply.github.com>
2023-06-13 11:33:14 +0200

    fix users roles auth (#57)

--- src/main/java/com/protify/Protify/controllers/PlaylistController.java
+++ src/main/java/com/protify/Protify/controllers/PlaylistController.java
@@ -26,9 +26,10 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.afford;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@RestController @SecurityRequirement(name="security_auth")
+@RestController
+@SecurityRequirement(name = "security_auth")
 @ExposesResourceFor(Playlist.class)
-@RequestMapping(value ="/playlists", produces = {MediaTypes.HAL_JSON_VALUE, MediaTypes.HAL_FORMS_JSON_VALUE})
+@RequestMapping(value = "/playlists", produces = {MediaTypes.HAL_JSON_VALUE, MediaTypes.HAL_FORMS_JSON_VALUE})
 @RequiredArgsConstructor
 
 public class PlaylistController {
@@ -40,7 +41,7 @@ public class PlaylistController {
     private final SongsModelAssembler songsModelAssembler;
 
     @GetMapping
-    @Operation(summary="Get Playlist list")
+    @Operation(summary = "Get Playlist list")
     public CollectionModel<EntityModel<Playlist>> getPlaylist(@ParameterObject Pageable page, @RequestParam(required = false, name = "page") Integer p,
                                                               @RequestParam(required = false) Integer size,
                                                               @RequestParam(required = false) String[] sort
@@ -48,66 +49,61 @@ public class PlaylistController {
 
         Page<Playlist> playlistPage = playlistService.getPlaylist(page);
         return pagedResourcesAssembler.toModel(playlistPage, playlistModelAssembler)
-                .mapLink(IanaLinkRelations.SELF, link->link.andAffordance(
-                        afford(methodOn(PlaylistController.class).addSinglePlaylist(null))
+                .mapLink(IanaLinkRelations.SELF, link -> link.andAffordance(
+                                afford(methodOn(PlaylistController.class).addSinglePlaylist(null))
                         )
-                        );
+                );
     }
 
     @GetMapping("{id}")
-    @Operation(summary="Get Playlist")
-    public EntityModel<Playlist> getSinglePlaylist(@PathVariable("id") Long id){
+    @Operation(summary = "Get Playlist")
+    public EntityModel<Playlist> getSinglePlaylist(@PathVariable("id") Long id) {
         Playlist playlist = playlistService.getSinglePlaylist(id);
         return playlistModelAssembler.toModel(playlist);
     }
 
     @GetMapping("{id}/songs")
-    @Operation(summary="Get Playlist's Song list")
+    @Operation(summary = "Get Playlist's Song list")
     public PagedModel<EntityModel<ScoredSongDto>> getPlaylistSongs(@PathVariable("id") Long id, @ParameterObject Pageable page, @RequestParam(required = false, name = "page") Integer p,
                                                                    @RequestParam(required = false) Integer size,
                                                                    @RequestParam(required = false) String[] sort) {
         Page<Songs> songsPage = songService.getSongsByPlaylist(id, page);
         PagedModel<EntityModel<ScoredSongDto>> songPage = songsPagedResourcesAssembler.toModel(songsPage, songsModelAssembler);
-        songPage.getContent().forEach((song) ->song.mapLink(IanaLinkRelations.SELF, link -> link.andAffordance(afford(methodOn(PlaylistController.class).deleteSongFromPlaylist(null)))));
+        songPage.getContent().forEach((song) -> song.mapLink(IanaLinkRelations.SELF, link -> link.andAffordance(afford(methodOn(PlaylistController.class).deleteSongFromPlaylist(null)))));
         return songPage;
     }
 
     @PostMapping
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#id).user.login == authentication.principal.username")
-    @Operation(summary="Create Playlist")
-    public Playlist addSinglePlaylist(@RequestBody PlaylistDto playlist){
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#id).user.login == authentication.name")
+    @Operation(summary = "Create Playlist")
+    public Playlist addSinglePlaylist(@RequestBody PlaylistDto playlist) {
         return playlistService.addSinglePlaylist(playlist);
     }
 
     @PatchMapping("{id}")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#id).user.login == authentication.principal.username")
-    @Operation(summary="Update Playlist")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#id).user.login == authentication.name")
+    @Operation(summary = "Update Playlist")
     public ResponseEntity<Playlist> updateSinglePlaylist(@PathVariable("id") Long id, @RequestBody PlaylistTitleDto playlistTitleDto) {
         return ResponseEntity.of(playlistService.updateSinglePlaylist(id, playlistTitleDto));
     }
 
     @DeleteMapping("/songs/delete")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#playlist.playlistId).user.login == authentication.principal.username")
-    @Operation(summary="From Playlist delete Song")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#playlist.playlistId).user.login == authentication.name")
+    @Operation(summary = "From Playlist delete Song")
     public ResponseEntity<Playlist> deleteSongFromPlaylist(@RequestBody PlaylistSongsDto playlist) {
         return ResponseEntity.of(playlistService.deleteSongFromPlaylist(playlist));
     }
 
     @PostMapping("/songs/add")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#playlist.playlistId).user.login == authentication.principal.username")
-    @Operation(summary="To Playlist add Song")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#playlist.playlistId).user.login == authentication.name")
+    @Operation(summary = "To Playlist add Song")
     public ResponseEntity<Playlist> addSongToPlaylist(@RequestBody PlaylistSongsDto playlist) {
         return ResponseEntity.of(playlistService.addSongToPlaylist(playlist));
     }
 
     @DeleteMapping("{id}")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#id).user.login == authentication.principal.username")
-    @Operation(summary="Delete Playlist")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @songService.getSingleSong(#id).user.login == authentication.name")
+    @Operation(summary = "Delete Playlist")
     public ResponseEntity<Playlist> deleteSinglePlaylist(@PathVariable("id") Long id) {
         playlistService.deleteSinglePlaylist(id);
         return ResponseEntity.ok(null);
--- src/main/java/com/protify/Protify/controllers/UserController.java
+++ src/main/java/com/protify/Protify/controllers/UserController.java
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.mapstruct.factory.Mappers;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -31,9 +32,12 @@ import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-@RestController @SecurityRequirement(name="security_auth")
+@RestController
+@SecurityRequirement(name = "security_auth")
 @ExposesResourceFor(User.class)
 @RequestMapping(value = "/users", produces = {MediaTypes.HAL_JSON_VALUE, MediaTypes.HAL_FORMS_JSON_VALUE})
 @RequiredArgsConstructor
@@ -54,18 +58,17 @@ public class UserController {
     @NonNull
     private final PlaylistModelAssembler playlistModelAssembler;
 
-    private final UserRepository userRepository;
-
-
     @GetMapping
-    @Operation(summary="User list")
+    @Operation(summary = "User list")
     public PagedModel<EntityModel<User>> getUser(@ParameterObject Pageable pageable, @RequestParam(required = false) Integer page,
                                                  @RequestParam(required = false) Integer size,
                                                  @RequestParam(required = false) String[] sort) {
         Page<User> users = userService.findAll(pageable);
         PagedModel<EntityModel<User>> model = pagedUserAssembler.toModel(users, userModelAssembler);
 
-        model.   mapLink(IanaLinkRelations.SELF, link ->
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+
+        model.mapLink(IanaLinkRelations.SELF, link ->
                 link.andAffordance(afford(methodOn(UserController.class).postUser(null, null)))
         );
 
@@ -73,8 +76,8 @@ public class UserController {
     }
 
     @PostMapping
-    @Operation(summary="Create User")
-    public ResponseEntity<EntityModel<User>> postUser(@RequestBody @Valid UserDto data, @RequestHeader(required = false)  @Parameter(hidden = true) String Accept) {
+    @Operation(summary = "Create User")
+    public ResponseEntity<EntityModel<User>> postUser(@RequestBody @Valid UserDto data, @RequestHeader(required = false) @Parameter(hidden = true) String Accept) {
         User user = userService.save(Mappers.getMapper(UserMapper.class).create(data));
 
         return ResponseEntity.created(
@@ -84,8 +87,7 @@ public class UserController {
 
 
     @GetMapping("{id}")
-    @Operation(summary="Get User")
-
+    @Operation(summary = "Get User")
     public EntityModel<User> getSingleUser(@PathVariable("id") Long id) {
         User user = userService.getSingle(id);
         return userModelAssembler.toModel(user);
@@ -93,8 +95,7 @@ public class UserController {
 
 
     @GetMapping("{id}/playlists")
-    @Operation(summary="Get User's Playlist list")
-
+    @Operation(summary = "Get User's Playlist list")
     public PagedModel<EntityModel<Playlist>> getPlaylists(@PathVariable Long id, @ParameterObject Pageable pageable, @RequestParam(required = false) Integer page,
                                                           @RequestParam(required = false) Integer size,
                                                           @RequestParam(required = false) String[] sort) {
@@ -103,9 +104,8 @@ public class UserController {
     }
 
     @DeleteMapping("{id}")
-    @Operation(summary="Delete User")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @userService.getSingle(#id).login == authentication.principal.username")
+    @Operation(summary = "Delete User")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @userService.getSingle(#id).login == authentication.name")
     public ResponseEntity<EntityModel<User>> deleteUser(@PathVariable("id") Long id, @RequestHeader(required = false) @Parameter(hidden = true) String Accept) {
         var user = userService.getSingle(id);
         userService.delete(user);
@@ -119,9 +119,8 @@ public class UserController {
     }
 
     @PutMapping("{id}")
-    @Operation(summary="Update User")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or @userService.getSingle(#id).login == authentication.principal.username")
+    @Operation(summary = "Update User")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @userService.getSingle(#id).login == authentication.name")
     public ResponseEntity<EntityModel<User>> putUser(@PathVariable Long id, @RequestBody @Valid UserDto body, @RequestHeader(required = false) @Parameter(hidden = true) String Accept) {
 
         User user = Mappers.getMapper(UserMapper.class).create(body);
@@ -136,10 +135,9 @@ public class UserController {
     }
 
     @PatchMapping("{id}")
-    @Operation(summary="Update User")
-    
-    @PreAuthorize("hasRole('ROLE_ADMIN') or #user.login == authentication.principal.username")
-    public ResponseEntity<EntityModel<User>> patchUser(@PathVariable("id") @Parameter(schema = @Schema(type="integer")) User user, @Valid @RequestBody UserDto body, @RequestHeader(required = false) @Parameter(hidden=true) String Accept) {
+    @Operation(summary = "Update User")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #user.login == authentication.name")
+    public ResponseEntity<EntityModel<User>> patchUser(@PathVariable("id") @Parameter(schema = @Schema(type = "integer")) User user, @Valid @RequestBody UserDto body, @RequestHeader(required = false) @Parameter(hidden = true) String Accept) {
         Mappers.getMapper(UserMapper.class).update(user, body);
 
         user = userService.save(user);
